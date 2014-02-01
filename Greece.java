04e513caa6a32eed98f615eedf667fbd2c70c734
Matthew Isom <isoma-13@rhodes.edu>
2014-02-01 14:54:07 -0600

    Revert "Added forgotten recipe"
    This reverts commit 23233fbd2650379e7643cdbdc528d9c546644dcb.

--- Greece.java
+++ Greece.java
@@ -443,12 +443,11 @@ public class Greece {
                 
                 GameRegistry.registerItem(hotBakingCover, "hotBakingCover");
                 LanguageRegistry.addName(hotBakingCover, "Hot Baking Cover");
-                GameRegistry.addSmelting(unfiredBakingCover.itemID, new ItemStack(hotBakingCover), 1);
-                GameRegistry.addShapelessRecipe(new ItemStack(Item.bread, 1), new ItemStack(hotBakingCover, 1, OreDictionary.WILDCARD_VALUE), Item.wheat, Item.wheat, Item.wheat);
                 
                 GameRegistry.registerItem(bakingCover, "bakingCover");
                 LanguageRegistry.addName(bakingCover, "Baking Cover");
-                GameRegistry.addSmelting(bakingCover.itemID, new ItemStack(hotBakingCover), 1);
+                GameRegistry.addSmelting(unfiredBakingCover.itemID, new ItemStack(hotBakingCover), 1);
+                GameRegistry.addShapelessRecipe(new ItemStack(Item.bread, 1), new ItemStack(hotBakingCover, 1, OreDictionary.WILDCARD_VALUE), Item.wheat, Item.wheat, Item.wheat);
                 
                 // REGISTER FOOD
                 GameRegistry.registerItem(olives, "olives");
